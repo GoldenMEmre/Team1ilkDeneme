3a65b1e71b2b02a12f0596d22188260fca18f179
GoldenMEmre <goldenpie60@gmail.com>
2023-05-22 23:55:28 +0300

    first commit

---
+++ src/test/java/OrtakClasss.java
@@ -0,0 +1,15 @@
+public class OrtakClasss {
+    public static void main(String[] args) {
+        System.out.println("ilk satırımız-Team Lead");
+
+
+    }
+}
+/*
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/GoldenMEmre/Team1ilkDeneme.git
+git push -u origin main
+ */
