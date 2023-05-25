21dfea81f0078c5b25811ea0b375464db3350e38
zdunal <125220288+zdunal@users.noreply.github.com>
2023-05-25 23:30:12 +0200

    Didem2505/V1

--- src/test/java/Tests/Didem.java
+++ src/test/java/Tests/Didem.java
@@ -1,9 +1,34 @@
 package Tests;
 
+import Pages.AmazonPage;
+import Utilities.ConfigReader;
+import Utilities.Driver;
+import org.testng.annotations.Test;
+
 public class Didem {
 
-    public static void main(String[] args) {
+    @Test
+    public void test01(){
 
-        System.out.println("didem hata yapti");
+        AmazonPage amazonPage = new AmazonPage();
+
+        // Amazon'a gidilir.
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        //Arama çubuğuna (istenen element) yazılır.
+        //Arama butonuna basılır.
+        amazonPage.aramaKutusu.sendKeys("Selenium");
+        amazonPage.aramaKutusu.submit();
+
+        //Browser kapatılır.
+        Driver.quitDriver();
+
+        //Not: 27.05.2023 tarihine kadar görev tamamlanıp Github'a gönderilmesi gerekir.
+
+
     }
+
+
+
+
 }
