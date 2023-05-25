cad593321b9e7ed8545fd524d02102153feb15ff
Mehmet-Akin <makin1625@gmail.com>
2023-05-25 23:27:05 +0200

    Mehmet2505/Amazon

--- src/test/java/Tests/makin.java
+++ src/test/java/Tests/makin.java
@@ -1,9 +1,32 @@
 package Tests;
 
-public class makin {
-    public static void main(String[] args) {
+import Pages.AmazonPage;
+import Utilities.ConfigReader;
+import Utilities.Driver;
+import org.testng.annotations.Test;
 
-        System.out.println(" bende geldim");
+public class makin {
+
+
+    @Test
+    public void AmazonTest(){
+
+        //1- Amazon'a gidilir.
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        //2-Arama çubuğuna (istenen element) yazılır.
+
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("Java");
+        //3-Arama butonuna basılır.
+        amazonPage.aramaKutusu.submit();
+
+        //4-Browser kapatılır.
+        Driver.closeDriver();
+
+
+        //Not: 27.05.2023 tarihine kadar görev tamamlanıp Github'a gönderilmesi gerekir.
+    }
 
     }
-}
+
