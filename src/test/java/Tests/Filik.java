d84a855744d81079c5cc9874c69b1382ba763bc1
mehmetfilik <filikmehmet02gmail.com>
2023-05-25 23:02:47 +0200

    Filik2505/V1

--- src/test/java/Tests/Filik.java
+++ src/test/java/Tests/Filik.java
@@ -1,7 +1,23 @@
 package Tests;
 
+import Pages.AmazonPage;
+import Utilities.ConfigReader;
+import Utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
 public class Filik {
-    public static void main(String[] args) {
-        System.out.println("Yasasin");
+
+    @Test
+    public void testFilik(){
+        //Amazon'a gidilir.
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        //Arama çubuğuna (istenen element) yazılır.
+        //Arama butonuna basılır.
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys(ConfigReader.getProperty("amazonAranacakKelime")+Keys.ENTER);
+        //Browser kapatılır.
+        Driver.closeDriver();
+        //Not: 27.05.2023 tarihine kadar görev tamamlanıp Github'a gönderilmesi gerekir.
     }
 }
