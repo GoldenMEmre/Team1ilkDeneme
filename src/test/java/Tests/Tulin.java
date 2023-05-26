700f8179db1da77b6c0f4fed2c875c74c67070cd
tulinbilgic <tulinbilgich@gmail.com>
2023-05-26 14:43:57 +0200

    tulin2605/V1

--- src/test/java/Tests/Tulin.java
+++ src/test/java/Tests/Tulin.java
@@ -1,7 +1,25 @@
 package Tests;
 
+import Pages.AmazonPage;
+import Utilities.ConfigReader;
+import Utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
 public class Tulin {
-    public static void main(String[] args) {
-        System.out.println("Tests.Tulin yaziyor");
+    /*
+    Amazon'a gidilir.
+ Arama çubuğuna (istenen element) yazılır.
+ Arama butonuna basılır.
+ Browser kapatılır.
+     */
+    @Test
+    public void test() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys(ConfigReader.getProperty("amazonAranacakKelime") +
+                Keys.ENTER);
+        Driver.closeDriver();
+
     }
 }
